3dfd9c29292df56da0fb9c094e67190644a5d539
Олександр <Pioner322622@gmail.com>
2023-08-07 14:08:12 +0300

    task 1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import re
+
+# task 1
+
+# try:
+#
+#     with open("input_text.txt", "r") as input_text:
+#         content_text = input_text.read()
+#
+#     text_filter = re.findall(r'\b\w{7,}\b', content_text)
+#
+#     with open("output_text.txt", "w") as output_text:
+#         output_text.write(' '.join(text_filter))
+#
+#     print("Successfully created a new file 'output_text.txt' with matching words")
+#
+# except IOError:
+#     print("Unable to read or write files!")
+
+
+
 
